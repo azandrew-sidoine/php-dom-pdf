22499871e15ca7df4779ea30286d1443f16a157f
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-10-21 18:10:58 +0000

    Rename storage path call to drewlabs_packages_dompdf_storage_path

--- helpers.php
+++ helpers.php
@@ -1,15 +1,18 @@
 <?php
 
 
-if (!function_exists('storage_path')) {
+if (!function_exists('drewlabs_packages_dompdf_storage_path')) {
     /**
      * Get the path to the storage folder.
      *
      * @param  string  $path
      * @return string
      */
-    function storage_path($path = '')
+    function drewlabs_packages_dompdf_storage_path($path = '')
     {
+        if (function_exists('storage_path')) {
+            return call_user_func('storage_path', $path);
+        }
         if (class_exists('\\Illuminate\\Container\\Container')) {
             return forward_static_call(array('\\Illuminate\\Container\\Container', 'getInstance'))->make('path.storage') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
         }
