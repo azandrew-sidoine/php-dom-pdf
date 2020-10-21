2079f805a68dda54aff646ce999b436c6a29a70c
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-10-21 18:07:35 +0000

    Modified storage_method_call

--- helpers.php
+++ helpers.php
@@ -10,6 +10,9 @@ if (!function_exists('storage_path')) {
      */
     function storage_path($path = '')
     {
-        \Illuminate\Container\Container::getInstance()->make('path.storage') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        if (class_exists('\\Illuminate\\Container\\Container')) {
+            return forward_static_call(array('\\Illuminate\\Container\\Container', 'getInstance'))->make('path.storage') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        }
+        throw new \RuntimeException(sprintf('%s call requires %s package', __FUNCTION__, 'illuminate/container'));
     }
 }
