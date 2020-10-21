8c9b97d37dd19ae0be52b362acdac3fbeed6e1ed
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-10-21 18:28:05 +0000

    Remname storage_path to drewlabs_packages_dompdf_storage_path

--- helpers.php
+++ helpers.php
@@ -1,15 +1,21 @@
 <?php
 
 
-if (!function_exists('storage_path')) {
+if (!function_exists('drewlabs_packages_dompdf_storage_path')) {
     /**
      * Get the path to the storage folder.
      *
      * @param  string  $path
      * @return string
      */
-    function storage_path($path = '')
+    function drewlabs_packages_dompdf_storage_path($path = '')
     {
-        \Illuminate\Container\Container::getInstance()->make('path.storage') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        if (function_exists('storage_path')) {
+            return call_user_func('storage_path', $path);
+        }
+        if (class_exists('\\Illuminate\\Container\\Container')) {
+            return forward_static_call(array('\\Illuminate\\Container\\Container', 'getInstance'))->make('path.storage') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        }
+        throw new \RuntimeException(sprintf('%s call requires %s package', __FUNCTION__, 'illuminate/container'));
     }
 }
--- src/Dompdf.php
+++ src/Dompdf.php
@@ -11,7 +11,8 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 /**
  * @package [[Drewlabs\Core\Dompdf]]
  */
-class Dompdf implements DomPdfable {
+class Dompdf implements DomPdfable
+{
 
 
     /**
@@ -31,7 +32,8 @@ class Dompdf implements DomPdfable {
     /**
      * @param PHPDomPdf $dompdf
      */
-    public function __construct(PHPDomPdf $dompdf){
+    public function __construct(PHPDomPdf $dompdf)
+    {
         $this->dompdf = $dompdf;
     }
 
@@ -41,7 +43,8 @@ class Dompdf implements DomPdfable {
      *
      * @return PHPDomPdf
      */
-    public function getDOMPdfProvider(){
+    public function getDOMPdfProvider()
+    {
         return $this->dompdf;
     }
 
@@ -52,7 +55,8 @@ class Dompdf implements DomPdfable {
      * @param string $orientation
      * @return $this
      */
-    public function setPaperOrientation($paper, $orientation = 'portrait'){
+    public function setPaperOrientation($paper, $orientation = 'portrait')
+    {
         $this->dompdf->setPaper($paper, $orientation);
         return $this;
     }
@@ -65,7 +69,8 @@ class Dompdf implements DomPdfable {
      * @param string $encoding Not used yet
      * @return static
      */
-    public function loadHTML($string, $encoding = null){
+    public function loadHTML($string, $encoding = null)
+    {
         $string = $this->transformSpecialCharacters($string);
         $this->dompdf->loadHtml($string, $encoding);
         $this->rendered = false;
@@ -77,7 +82,8 @@ class Dompdf implements DomPdfable {
      * @param string $file
      * @return static
      */
-    public function loadFile($file){
+    public function loadFile($file)
+    {
         $this->dompdf->loadHtmlFile($file);
         $this->rendered = false;
         return $this;
@@ -89,8 +95,9 @@ class Dompdf implements DomPdfable {
      * @param array $info
      * @return static
      */
-    public function addInfo($info){
-        foreach($info as $name=>$value){
+    public function addInfo($info)
+    {
+        foreach ($info as $name => $value) {
             $this->dompdf->add_info($name, $value);
         }
         return $this;
@@ -102,7 +109,8 @@ class Dompdf implements DomPdfable {
      * @param array $options
      * @return static
      */
-    public function setPHPDomPdfOptions(array $options) {
+    public function setPHPDomPdfOptions(array $options)
+    {
         $options = new Options($options);
         $this->dompdf->setOptions($options);
         return $this;
@@ -113,8 +121,9 @@ class Dompdf implements DomPdfable {
      *
      * @return string The rendered PDF as string
      */
-    public function printDocument(){
-        if(!$this->rendered){
+    public function printDocument()
+    {
+        if (!$this->rendered) {
             $this->render();
         }
         return $this->dompdf->output();
@@ -127,7 +136,8 @@ class Dompdf implements DomPdfable {
      * @param $flags
      * @return static
      */
-    public function writeDocument($filePath, $flags = null){
+    public function writeDocument($filePath, $flags = null)
+    {
         file_put_contents($filePath, $this->printDocument(), $flags ? LOCK_EX : 0);
         return $this;
     }
@@ -139,14 +149,15 @@ class Dompdf implements DomPdfable {
      * @param string $disposition
      * @return BinaryFileResponse
      */
-    public function download($filename = 'document.pdf', $disposition = 'attachment') {
-        $filePath = storage_path("app" . DIRECTORY_SEPARATOR . uniqid() . "-$filename");
+    public function download($filename = 'document.pdf', $disposition = 'attachment')
+    {
+        $filePath = \drewlabs_packages_dompdf_storage_path("app" . DIRECTORY_SEPARATOR . uniqid() . "-$filename");
         $this->writeDocument($filePath);
         $response = new BinaryFileResponse($filePath, 200, array(
             'Content-Type' => 'application/pdf'
         ), true);
 
-        if (! is_null($filename)) {
+        if (!is_null($filename)) {
             return $response->setContentDisposition($disposition, $filename, 'document.pdf');
         }
         return $response->deleteFileAfterSend(true);
@@ -158,7 +169,8 @@ class Dompdf implements DomPdfable {
      * @param \Closure $callback
      * @return StreamedResponse
      */
-    public function streamDownload($filename = 'document.pdf', $callback = null, $disposition = 'attachment') {
+    public function streamDownload($filename = 'document.pdf', $callback = null, $disposition = 'attachment')
+    {
         return $this->dompdf->stream($filename);
         // $response = new StreamedResponse($callback, 200, array(
         //     'Content-Type' => 'application/pdf'
@@ -178,25 +190,28 @@ class Dompdf implements DomPdfable {
     /**
      * Render the PDF document
      */
-    protected function render(){
-        if(!$this->dompdf){
+    protected function render()
+    {
+        if (!$this->dompdf) {
             throw new Exception('PDF Provider not initialized');
         }
         $this->dompdf->render();
         $this->rendered = true;
     }
 
-    public function setEncryption($password) {
-       if (!$this->dompdf) {
-           throw new Exception("PDF Provider not initialized");
-       }
-       $this->render();
-       return $this->dompdf->getCanvas()->{'get_cpdf'}()->setEncryption("pass", $password);
+    public function setEncryption($password)
+    {
+        if (!$this->dompdf) {
+            throw new Exception("PDF Provider not initialized");
+        }
+        $this->render();
+        return $this->dompdf->getCanvas()->{'get_cpdf'}()->setEncryption("pass", $password);
     }
 
 
-    protected function transformSpecialCharacters($subject){
-        foreach(array('€' => '&#0128;','£' => '&pound;') as $search => $replace){
+    protected function transformSpecialCharacters($subject)
+    {
+        foreach (array('€' => '&#0128;', '£' => '&pound;') as $search => $replace) {
             $subject = str_replace($search, $replace, $subject);
         }
         return $subject;
